f06e0a6b6b156c87a333e8b1a18cafd729e07a2d
Ngoc Pham <idontgotit@gmail.com>
2022-06-19 23:54:19 +0700

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,7 +1,3 @@
-fn main() {
-    println!("Hello, world!");
-}
-
 
 // Bài tập
 // Cho ngữ cảnh như sau : Một ngôi trường cần lập danh
@@ -18,20 +14,23 @@ fn main() {
 // }
 
   
-GỢI Ý KHÔNG HỢP LÝ.
-TÊN SINH VIÊN VÀ ĐIỂM CỦA SINH VIÊN ĐÓ:
-- 1 sinh viên sẽ có nhiều môn học, Cấu trúc phải thiết kế sao cho lưu được điểm của tất cả các môn của cùng 1 sinh viên.
-- Lưu thế này sẽ duplicate tên của sinh viên rất nhiều lần, khi bổ sung 1 môn học mới.
-- Các sinh viên ko có key unique để phân biệt. Nếu cùng tên cùng điểm số, thì ko biết ai là ai luôn.
-- Do ko có key unique, giả sử nhập nhầm tên sinh viên, là cũng ko biết sửa chỗ nào cho đúng luôn.
+// GỢI Ý CẤU TRÚC struct School KHÔNG HỢP LÝ. <=======
+
+// - 1 sinh viên sẽ có nhiều môn học, cấu trúc phải thiết kế sao cho lưu được điểm của tất cả các môn của cùng 1 sinh viên.
+// - Lưu thế này sẽ duplicate tên của sinh viên rất nhiều lần, khi bổ sung 1 môn học mới.
+// - Các sinh viên ko có key unique để phân biệt. Nếu cùng tên cùng điểm số, thì ko biết ai là ai luôn.
+// - Do ko có key unique, giả sử người dùng nhập nhầm tên sinh viên, thì cũng ko biết sửa vào ai cho đúng.
 
 
-cấu trúc hợp lý dự kiến như sau:
+// cấu trúc hợp lý dự kiến như sau:
+// Subject là thành phần nhỏ nhất
+// Student bao gồm nhiều subject
+// School bao gồm nhiều student
 
-struct Student {
-    name: String,
-    student_id: String,
-}
+
+
+use std::collections::HashMap;
+
 
 struct Subject {
     subject_name: String,
@@ -39,18 +38,14 @@ struct Subject {
     student_id: String,
 }
 
-impl Student {
-    pub fn new() -> Student {
-        Student {
-            name: String::new(),
-            student_id: String::new(),
-        }
-    }
+struct Student {
+    name: String,
+    student_id: String,
+    subjects: Vec<Subject>,
+}
 
-    pub fn add(&mut self, name: &str, student_id: &str) {
-        self.name = name;
-        self.student_id=student_id;
-    }
+struct School {
+    students: Vec<Student>,
 }
 
 impl Subject {
@@ -63,22 +58,100 @@ impl Subject {
     }
 
     pub fn add(&mut self, subject_name: &str, student_id: &str, score: &str) {
-        self.name = name;
-        self.student_id=student_id;
-        self.score=score;
+        self.subject_name = subject_name.to_string();
+        self.student_id=student_id.to_string();
+        self.score=score.to_string();
     }
-    
+}
+
+impl Student {
+    pub fn new() -> Student {
+        Student {
+            name: String::new(),
+            student_id: String::new(),
+            subjects: Vec::new(),
+        }
+    }
+
+    pub fn add(&mut self, name: &str, student_id: &str, subjects: Vec<Subject>) {
+        self.name = name.to_string();
+        self.student_id=student_id.to_string();
+        self.subjects = subjects;
+    }
+}
+
+
+impl School {
+    pub fn new() -> School {
+        School {
+            students: Vec::new(),
+        }
+    }
+
+    pub fn add(&mut self, students: Vec<Student>) {
+        self.students = students;
+        
+    }
+
     // //2. Liệt kê các điểm số hiện tại mà trường đã cập nhật. Yêu cầu này chả có lý do gì phải trả về tên học sinh là gì cả. Tất cả điểm số unique là được
     
-    pub fn grades(&self) -> Vec<u32> {
-        unimplemented!()
+    pub fn scores(&self) -> Vec<String>{
+        let mut result:Vec<String> = vec![];
+        for student in self.students.iter() {
+            
+            for subject in student.subjects.iter() {
+                result.push(subject.score.to_string())
+            }
+        }
+
+        println!("{:?}", result);
+        return result
 
     }
     
     //3. Liệt kê danh sách các học sinh có cùng 1 điểm số
     
-    pub fn grade(&self, score: u32) -> Vec<String> {
-        unimplemented!()
+    pub fn scores_with_names(&self, score: &str) -> Vec<String> {
+        let mut result:Vec<String> = vec![];
+        for student in self.students.iter() {
+            for subject in student.subjects.iter() {
+                if subject.score == score{
+                    result.push(student.name.to_string())
+                }
+            }
+        }
+
+        println!("{:?}", result);
+        return result
         
     }
+}
+
+fn main() {
+    println!("Hello, VBI-Rust-Workshop-3!");
+
+    let mut subject_math_of_alice: Subject= Subject::new();
+    let mut subject_math_of_bob: Subject= Subject::new();
+    let mut subject_math_of_charlie: Subject= Subject::new();
+    subject_math_of_alice.add("Math", "MSSV_123", "3");
+    subject_math_of_bob.add("Math", "MSSV_123", "10");
+    subject_math_of_charlie.add("Math", "MSSV_123", "4");
+
+    let mut student_alice: Student= Student::new();
+    let mut student_bob: Student= Student::new();
+    let mut student_charlie: Student= Student::new();
+    student_alice.add("Alice", "MSSV_123", vec![subject_math_of_alice]);
+    student_bob.add("Bob", "MSSV_456", vec![subject_math_of_bob]);
+    student_charlie.add("Charlie", "MSSV_890", vec![subject_math_of_charlie]);
+
+
+    let mut school_a: School= School::new();
+    let mut vec_students = vec![student_alice, student_bob, student_charlie];
+    school_a.add(vec_students);
+
+    school_a.scores();
+
+    school_a.scores_with_names("4");
+
+
 }
